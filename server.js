eff062b42e69ec7280a3afe15a978e6a2cdae71e
Prajwal Shrestha <pjmessi25@gmail.com>
2020-12-16 22:09:44 +0545

    added procfile and fixed bug in our server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const serverStatic = require('serve-static')
+const path = require('path')
+
 const port = process.env.PORT || 8080;
+const app = express();
 
-const app = express()
+app.use('/', serverStatic(path.join(__dirname, '/dist')))
 
-app.use(express.static(__dirname+"/dist/index.html"))
-app.listen('Server started...')
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
 
